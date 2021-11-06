941eb40ce995bb4c8067b53a1d2c3d973d8fae18
Eduard Muliar <edikmulyar228@gmail.com>
2021-11-06 03:02:21 +0200

    Created a new folder 'js', add section ' projects content' and footer

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+let nav_offset_top = $('.header__menu').height();
+
+function navbarFixed(){
+    if($('.header__menu').length){
+        $(window).scroll(function() {
+            let scroll = $(window).scrollTop();
+            if(scroll > nav_offset_top){
+                $('.header__menu').addClass('navbar__fixed');
+            }
+            else{
+                $('.header__menu').removeClass('navbar__fixed');
+            }
+        });
+    };
+};
+
+navbarFixed();
